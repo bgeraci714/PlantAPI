7c96c5e72e674a237e4d7d42c92f65ec09e562df
Blake Geraci <bgeraci851@hcc.edu>
2016-11-28 01:18:26 -0500

    Re-implemented the api controller using non-generic database calls to the Plants DB

--- app/controllers/api/plants_controller.rb
+++ app/controllers/api/plants_controller.rb
@@ -1,13 +1,56 @@
 module Api
-  class PlantsController < Api::BaseController
+  class PlantsController < ApplicationController
+    protect_from_forgery with: :null_session
+    before_action :set_plant, only: [:destroy, :show, :update]
+    respond_to? :json
+    skip_before_action :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+
     private
       def plant_params
         params.require(:plant).permit(:name, :description)
       end
 
-      def query_params
-        params.permit(:name, :description)
+      def set_plant
+        @plant = Plant.find(params[:id])
       end
 
+    public
+      # POST /api/plants
+      def create
+        @plant = Plant.new(plant_params)
+
+        if @plant.save
+          render :json => @plant, status: :created
+        else
+          render json: @plant.errors, status: :unproccessable_entity
+        end
+      end
+
+      # DELETE /api/plants/1
+      def destroy
+        @plant.destroy
+        head :no_content
+      end
+
+      # GET /api/plants
+      def index
+        @plants = Plant.all
+        render json: @plants
+      end
+
+
+      # GET /api/plants/1
+      def show
+        render json: @plant
+      end
+
+      # PATCH/PUT
+      def update
+        if @plant.update(plant_params)
+          render :show
+        else
+          render json: @plant.errors, status: :unprocessable_entity
+        end
+      end
   end
 end
